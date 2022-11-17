a358c697896fc6a7df366c11b92b5a037fef2f57
matsudamper <let@matsudamper.net>
2022-11-17 18:12:28 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            version("compose_compiler", "1.3.2")
+
+            library("android-core-ktx", "androidx.core:core-ktx:1.9.0")
+            library("android-activity-ktx", "androidx.activity:activity-ktx:1.6.1")
+            library("android-activity-compose", "androidx.activity:activity-compose:1.6.1")
+
+            val composeUiVersion = "1.2.0"
+            library("android-compose-ui", "androidx.compose.ui:ui:$composeUiVersion")
+            library("android-compose-ui-graphics", "androidx.compose.ui:ui-graphics:$composeUiVersion")
+            library("android-compose-ui-tooling", "androidx.compose.ui:ui-tooling:$composeUiVersion")
+            library("android-compose-ui-tooling-preview", "androidx.compose.ui:ui-tooling-preview:$composeUiVersion")
+            library("android-compose-ui-test-junit4", "androidx.compose.ui:ui-test-junit4:$composeUiVersion")
+            library("android-compose-ui-test-manifest", "androidx.compose.ui:ui-test-manifest:$composeUiVersion")
+        }
+    }
+}
+rootProject.name = "Android Test"
+include(":app")
