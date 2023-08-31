360473275fd78d561b207d6db9b04b5c02ae2346
matsudamper <matsudamper971227@gmail.com>
2023-08-31 15:18:07 +0900

    fix

--- build-logic/src/main/kotlin/VersionCatalog.kt
+++ build-logic/src/main/kotlin/VersionCatalog.kt
@@ -1,4 +1,5 @@
 
+
 object VersionCatalog {
     object Android {
         val androidKtx = "androidx.core:core-ktx:1.8.0"
@@ -6,11 +7,11 @@ object VersionCatalog {
         object Compose {
             private val composeUiVersion = "1.3.3"
             val ui = "androidx.compose.ui:ui:$composeUiVersion"
-            val uiGraphics" = "androidx.compose.ui:ui-graphics:$composeUiVersion"
-            val uiTooling" = "androidx.compose.ui:ui-tooling:1.3.3"
-            val uiToolingPreview" = "androidx.compose.ui:ui-tooling-preview:1.3.3"
-            val uiTestJunit4" = "androidx.compose.ui:ui-test-junit4:1.3.3"
-            val uiTestManifest" = "androidx.compose.ui:ui-test-manifest:1.3.3"
+            val uiGraphics = "androidx.compose.ui:ui-graphics:$composeUiVersion"
+            val uiTooling = "androidx.compose.ui:ui-tooling:1.3.3"
+            val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview:1.3.3"
+            val uiTestJunit4 = "androidx.compose.ui:ui-test-junit4:1.3.3"
+            val uiTestManifest = "androidx.compose.ui:ui-test-manifest:1.3.3"
         }
     }
 }
