5b9d57339c33f350abec805c02f4ade5b1335393
matsudamper <let@matsudamper.net>
2023-09-11 18:02:53 +0900

    add android gradle plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
+    id("com.android.application") version libs.versions.androidGradlePlugin.get() apply false
+    id("com.android.library") version libs.versions.androidGradlePlugin.get() apply false
     id("org.jetbrains.kotlin.android") version libs.versions.kotlin.get() apply false
 }
